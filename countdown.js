dfccd033badf00b38ec662bfb50a1f0d30b75652
Mert Efe Şükür <88542815+Mert-Efe-Sukur@users.noreply.github.com>
2024-11-13 18:24:29 +0300

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jun 02, 2024 09:30:42").getTime();
+var countDownDate = new Date("Dec 31, 2024 21:08:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
